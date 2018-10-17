3979f0a65e38b643a28ee947ddc65159246172ab
Kimbornino <kimbornino@gmail.com>
2018-10-17 12:38:55 -0500

    Added model for seasonal vegetables.

---
+++ Capstone/Capstone/Models/LocalFoods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class LocalFoods
+    {
+        public string FoodName { get; set; }
+
+        public bool January { get; set; }
+
+        public bool February { get; set; }
+
+        public bool March { get; set; }
+
+        public bool April { get; set; }
+
+        public bool May { get; set; }
+
+        public bool June { get; set; }
+
+        public bool July { get; set; }
+
+        public bool August { get; set; }
+
+        public bool September { get; set; }
+
+        public bool October { get; set; }
+
+        public bool Novemebr { get; set; }
+
+        public bool December { get; set; }
+        
+    }
+}
