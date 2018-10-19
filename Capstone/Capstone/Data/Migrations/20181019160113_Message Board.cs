82e7748326deb12e0265aacac324c5f43a8bbac1
Kimbornino <kimbornino@gmail.com>
2018-10-19 11:02:05 -0500

    Added migration.

---
+++ Capstone/Capstone/Data/Migrations/20181019160113_Message Board.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Capstone.Data.Migrations
+{
+    public partial class MessageBoard : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MessageBoard",
+                columns: table => new
+                {
+                    MessageBoardID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Topic = table.Column<string>(nullable: true),
+                    Message = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageBoard", x => x.MessageBoardID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MessageBoard");
+        }
+    }
+}
